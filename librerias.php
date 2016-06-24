055b2133ed45dfe07da9e65ba8d326a00d206e9f
Negrita <Negrita@Negrita-PC>
2016-06-23 20:05:29 -0400

    Merge origin/master
    Conflicts:
    	index.php

--- librerias.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include ('constantes.php');
-include (PATHAPP.'/lib/db_funciones.php');
-include (PATHAPP.'/clases/Usuario.php');
-?>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'librerias.php';
-session_start();
-session_unset();
-?>
-<script>
-	document.location.href="index.php";
-</script>
